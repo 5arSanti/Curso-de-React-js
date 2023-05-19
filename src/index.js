fd7ed5122e3cb942d07ce0d554f3283c59be9970
5arSanti <santiari05@hotmail.com>
2023-05-19 15:24:58 -0500

    Migracion a React 17.0.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+//import ReactDOM from 'react-dom';
+import { render} from "react-dom"
+
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+//React 17 encontrado por JuanDc
+const root = document.getElementById("root");
+render(<App/>, root);
+
+//React 17 encontrado por Santiago
+// ReactDOM.render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>,
+//    document.getElementById('root')
+//   );
+
+//React 18
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+
+//Consejos
+//Es importante aprender a cambiar de versiones entre React, tanto upgrade y downgrade
+//Es importante observar que beneficios puede traer una actualizacion de version de React
+//Ejemplo: Que la aplicacion cargue 1 segundo as rapido => (Beneficio para los usuarios y puede ser mejor opcion actualizar para conseguir ese segundo extra, que implementar cambios en el codigo para mejor rendimiendo y conseguir el segundo extra)
